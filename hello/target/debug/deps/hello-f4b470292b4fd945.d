011ad4fd1ce0b9aec1c2daa67ffdd1b6c18c40cc
soolmuk <72430756+soolmuk@users.noreply.github.com>
2023-01-10 19:48:34 +0900

    First

---
+++ hello/target/debug/deps/hello-f4b470292b4fd945.d
@@ -0,0 +1,5 @@
+/Users/ihoseong/Documents/Documents/Making_Rust/hello/target/debug/deps/hello-f4b470292b4fd945: src/main.rs
+
+/Users/ihoseong/Documents/Documents/Making_Rust/hello/target/debug/deps/hello-f4b470292b4fd945.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/Users/ihoseong/Documents/Documents/Making_Rust/hello/target/debug/hello: /Users/ihoseong/Documents/Documents/Making_Rust/hello/src/main.rs
